f80282ea8da72ec41f57774d2b28229ac6d67c84
shawnofthedev <svotaw81@gmail.com>
2023-02-06 07:50:57 -0500

    Add more tests

--- TestNinja.Nunit.UnitTests/FizzBuzzTests.cs
+++ TestNinja.Nunit.UnitTests/FizzBuzzTests.cs
@@ -6,19 +6,33 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class FizzBuzzTests
     {
-        private FizzBuzz _fizzBuzz;
 
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3And5_ReturnFizzBuzz()
         {
-            _fizzBuzz= new FizzBuzz();
+            var result = FizzBuzz.GetOutput(15);
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
         }
 
         [Test]
-        public void GetOutput_DivisibleByZero_ReturnsNumber()
+        public void GetOutput_InputIsDivisibleBy3Only_ReturnFizz()
         {
-            var result = _fizzBuzz.GetOutput(0);
-            Assert.That(result, Is.EqualTo(0));
+            var result = FizzBuzz.GetOutput(3);
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy5Only_ReturnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(5);
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsNotDivisibleBy3Or5_ReturnTheSameNumber()
+        {
+            var result = FizzBuzz.GetOutput(1);
+            Assert.That(result, Is.EqualTo("1"));
         }
     }
 }
--- TestNinja/Fundamentals/FizzBuzz.cs
+++ TestNinja/Fundamentals/FizzBuzz.cs
@@ -2,7 +2,7 @@
 {
     public class FizzBuzz
     {
-        public string GetOutput(int number)
+        public static string GetOutput(int number)
         {
             if ((number % 3 == 0) && (number % 5 == 0))
                 return "FizzBuzz";
